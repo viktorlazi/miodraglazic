1b534175b4c465053f6b7c159a8d0fcf7a600672
Viktorlazi <viktorlazic11@gmail.com>
2020-11-03 19:37:01 +0100

    stavia id-ove u array

--- order.php
+++ order.php
@@ -1,8 +1,25 @@
 <?php
-include_once('classes/db.php');
+    include_once('classes/db.php');
 
-$slike=($_POST["arr"]);
-print_r($slike);
+    $slike=($_POST["arr"]);
+    $slike=$slike.'.';
+    print_r($slike);
+
+    $s=array();
+    while(true){
+        if(strlen($slike)==0){
+             break;
+        }
+       else if(substr($slike,0,1) == '.'){
+        $slike = substr($slike, 1, strlen($slike));
+       }
+       else if(substr($slike,0,1) != '.'){
+           array_push($s,substr($slike,0,strpos($slike, '.')));
+           $slike = substr($slike, strpos($slike, '.'), strlen($slike));
+       }    
+    }
+    print_r($s);
+
 ?>
 
 
